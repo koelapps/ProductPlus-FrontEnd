2eb41bbd9c5ade5300cadf7aad21900e0ffc4ab0
MINGW64 <md.asad@koelapps.com>
2021-05-07 18:01:37 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import theme from './ui/Theme';
+import Header from '../components/ui/Header';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route exact path="/services" component={() => <div>Services</div>} />
+          <Route
+            exact
+            path="/customsoftware"
+            component={() => <div>CustomSoftware</div>}
+          />
+          <Route
+            exact
+            path="/mobileapps"
+            component={() => <div>MobileApps</div>}
+          />
+          <Route exact path="/websites" component={() => <div>Websites</div>} />
+          <Route
+            exact
+            path="/revolution"
+            component={() => <div>Revolution</div>}
+          />
+          <Route exact path="/about" component={() => <div>About US</div>} />
+          <Route
+            exact
+            path="/contact"
+            component={() => <div>Contact US</div>}
+          />
+          <Route exact path="/estimate" component={() => <div>Estimate</div>} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Theme.js
@@ -0,0 +1,32 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const prodBlue = '#0B72B9';
+const prodOrange = '#FFBA60';
+
+export default createMuiTheme({
+  palette: {
+    common: {
+      green: `${prodBlue}`,
+      orange: `${prodOrange}`,
+    },
+    primary: {
+      main: `${prodBlue}`,
+    },
+    secondary: {
+      main: `${prodOrange}`,
+    },
+  },
+  typography: {
+    tab: {
+      fontFamily: 'Raleway',
+      textTransform: 'none',
+      fontWeight: 700,
+      fontSize: '0.9rem',
+    },
+    estimate: {
+      fontFamily: 'pacifico',
+      fontSize: '1rem',
+      textTransform: 'none',
+    },
+  },
+});
